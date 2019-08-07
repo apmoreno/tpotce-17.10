d0a618916b4818e3263a2aacc301b623c75fc1b9
apmoreno <52453640+apmoreno@users.noreply.github.com>
2019-08-07 13:49:07 +0400

    Update pre-install.sh

--- iso/installer/pre-install.sh
+++ iso/installer/pre-install.sh
@@ -7,3 +7,9 @@ sudo locale-gen en_US.UTF-8
 # Update OS
 sudo apt update && apt upgrade -y 
 echo "Pre-install complete.\nUpdated locale and upgraded OS\n"
+
+
+git clone https://github.com/apmoreno/tpotce-17.10/
+cd ./iso/installer
+sudo su
+./install.sh
